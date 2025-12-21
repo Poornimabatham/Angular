995f24f66afd11f79ce93dd5c8a6793d163f20ff
Poornima Batham <pbatham21@gmail.com>
2025-12-21 14:58:01 +0530

    Ad coe

--- src/app/people/people.ts
+++ src/app/people/people.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-people',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './people.html',
   styleUrl: './people.css'
 })
-export class People {
+export class People implements OnInit {
+    Users: any[] = [];
+  isLoading = true  
+    filteredUsers: any[] = [];
+    searchTerm: string = '';
+  constructor(private http: HttpClient) { }
+  
+  ngOnInit(): void {
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe(
+      (data: any) => {
+        this.Users = data;
+                this.filteredUsers = data;
+
+        this.isLoading = false
+        console.log(data);
+      }
+    )
+  }
+
+  onSearch(): void {
+    this.filteredUsers = this.Users.filter(user => 
+      user.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+  getStatus(index: number): string {
+  const statuses = ['Delivered', 'Process', 'Canceled'];
+  return statuses[index % 3];
+}
+getStatusColor(index: number): string {
+  const status = this.getStatus(index);
+  switch(status) {
+    case 'Delivered': return '#4CAF50';
+    case 'Process': return '#FF9800';
+    case 'Canceled': return '#F44336';
+    default: return '#9E9E9E';
+  }
+}
+
+getStatusTextColor(index: number): string {
+  return this.getStatus(index) === 'Process' ? 'black' : 'white';
+}
 
 }
