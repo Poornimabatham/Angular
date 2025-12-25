b42f747f6c284e1ffdb40c3bbb1d1cb121528b6b
Poornima Batham <pbatham21@gmail.com>
2025-12-25 21:13:37 +0530

    add code

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,24 +3,16 @@ import { Router, RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
-import { Feature } from './feature/feature';
-import { Projects } from './projects/projects';
-import { UserManagement } from './user-management/user-management';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     Navbar,
-    Feature,
     MatIconModule,
     MatSlideToggleModule,
-    UserManagement,
     CommonModule
-    // <- add this
   ],
   templateUrl: './app.html',
   styleUrl: './app.css',
